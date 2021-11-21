7c377c8bc8fafb4d61c1cbd04845cd09cc49e88c
kalin11 <90985536+kalin11@users.noreply.github.com>
2021-11-21 23:49:28 +0300

    Add files via upload

---
+++ Donut.java
@@ -0,0 +1,11 @@
+package Donut;
+
+import Enums.TypesOfPeople;
+import Human.Human;
+
+public class Donut extends Human {
+    public Donut(){
+        super("Пончик", TypesOfPeople.GIANT, false);
+    }
+
+}
